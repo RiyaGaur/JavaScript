9cb8c53aca87272ba6e64dac0503994d17822ffc
RiyaGaur <riyagaur15@gmail.com>
2020-09-16 23:07:56 +0530

    Console Logs, Errors, Warnings, Comments

---
+++ JS/index.js
@@ -0,0 +1,18 @@
+console.time('Your code Took');
+console.log('Hello Console');
+console.log(3+4);
+console.log(64);
+console.log(true);
+console.log([11,12,13,14,15]);
+console.log({riya:'hi',marks:36});
+console.table({riya:'hi',marks:36});
+console.warn('This is a warning');
+console.timeEnd('Your code Took');
+console.error('This is an error');
+
+// Single Line Comment
+/*
+Multi 
+Line
+Comment
+*/
