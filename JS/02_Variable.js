c0f6b7b905dced0f74308f4dbd95a1b0ac523496
RiyaGaur <riyagaur15@gmail.com>
2020-09-17 11:58:36 +0530

    let, const & var

---
+++ JS/02_Variable.js
@@ -0,0 +1,24 @@
+var name = 'Riya';
+name = 'Riya Gaur';
+var sec;
+console.log(name,sec);
+sec='B';
+console.log(name,sec);
+
+
+const headname = 'Riya';
+// headname = 'Riya Gaur';       Not allowed Modification
+
+
+var city = 'Mathura';
+{
+    let city = 'Kolkata';
+    console.log(city);
+}
+console.log(city);
+
+const arr=[12,23,53,3];
+console.log(arr);
+arr.push(43);
+console.log(arr);
+// arr=[11,12,13];      Not Allowed
