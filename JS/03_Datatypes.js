85ccae566bc83cb354855b593c9ae20f0ba6802e
RiyaGaur <riyagaur15@gmail.com>
2020-09-17 14:08:03 +0530

    Data Types (Primitive & Reference Types)

---
+++ JS/03_Datatypes.js
@@ -0,0 +1,46 @@
+// Primitive Datatypes
+
+//String
+let name="Riya";
+console.log("Datatype is " + (typeof name));
+
+// Numbers
+let marks = 36;
+console.log("Datatype is " + typeof marks);
+
+// Boolean
+let isDriver = true;
+console.log("Datatype is " + typeof isDriver);
+
+// Null
+let nullvar = null;
+console.log("Datatype is " + typeof nullvar);
+
+// Undefined
+let undef = undefined;
+console.log("Datatype is " + typeof undef);
+
+// Reference Datatypes
+
+// Array
+let arr = [1,2,3,"4"];
+console.log("Datatype is " + typeof arr);
+
+// Objects
+let stdmarks = {
+    A: 34,
+    B: 36,
+    C: 40
+};
+console.log("Datatype is " + typeof stdmarks);
+
+// function
+
+function findname(){
+
+}
+console.log("Datatype is " + typeof findname);
+
+// Date
+let date = new Date();
+console.log("Datatype is " + typeof date);
