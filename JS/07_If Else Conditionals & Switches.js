89c9970d9f4ea8f8266d2c565385556f28736879
RiyaGaur <riyagaur15@gmail.com>
2020-09-17 18:06:22 +0530

    If Else Conditionals & Switches

---
+++ JS/07_If Else Conditionals & Switches.js
@@ -0,0 +1,55 @@
+let age = 128;
+const doesDrive = false;
+const vari = 34;
+
+if (age==19){
+    console.log('Age is not 19')
+}
+
+else if(age !== 65){
+    console.log('Age is not 65')
+}
+
+else{
+    console.log('Age is 128')
+}
+
+if (typeof vari !== 'undefined'){
+    console.log('Vari is defined');
+}
+
+else{
+    console.log('Vari is not defined');
+}
+
+if (doesDrive || age>18){
+    console.log("You can drive");
+}
+else{
+    console.log("You cannot drive");
+}
+
+// ternary operator
+
+console.log(age==45? 'Age is 45': 'Age is not 45');
+
+// Switch case
+
+age=18;
+switch (age) {
+    case 18:
+        console.log("You are 18");
+        break;
+
+    case 28:
+        console.log("You are 28");
+        break;
+
+    case 38:
+        console.log("You are 38");
+        break;
+
+    default:
+        console.log("You are unknown age");
+        break;
+};
