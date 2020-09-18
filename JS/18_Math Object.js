390d63d7e25347646b87878710ec8a67f0b63509
RiyaGaur <riyagaur15@gmail.com>
2020-09-18 23:34:56 +0530

    Math Object

---
+++ JS/18_Math Object.js
@@ -0,0 +1,42 @@
+console.log("Welcome to tutorial number 23");
+let x = 3;
+let y = 6;
+let z;
+z = x+y;
+console.log(z);
+z = x-y;
+console.log(z);
+z = x*y;
+console.log(z);
+z = x/y;
+console.log(z);
+
+// Exploring the Math object
+z = Math;
+console.log(z);
+z = Math.PI;
+console.log(z);
+z = Math.E;
+console.log(z);
+z = Math.round(5.4);
+console.log(z);
+z = Math.ceil(5.3);
+console.log(z);
+z = Math.floor(-5.3);
+console.log(z);
+z = Math.abs(-5);
+console.log(z);
+z = Math.sqrt(64);
+console.log(z);
+z = Math.pow(2, 3);
+console.log(z);
+z = Math.min(2, 3, 34,234, 2342,34);
+console.log(z);
+z = Math.max(2, 3, 34,234,34);
+console.log(z);
+z = Math.random();
+console.log(z);
+z = 100*Math.random()
+console.log(z);
+z = Math.ceil(50 + (100-50)*Math.random())
+console.log(z); 
