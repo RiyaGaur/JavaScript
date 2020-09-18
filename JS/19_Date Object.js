e996018f644ff830c885a0035e548169653521e7
RiyaGaur <riyagaur15@gmail.com>
2020-09-18 23:48:19 +0530

    Data & Time

---
+++ JS/19_Date Object.js
@@ -0,0 +1,37 @@
+let today = new Date();
+console.log(today);
+console.log(typeof today);
+let otherDate = new Date('8-4-2003 04:54:08');
+let a;
+a = otherDate.getDay();
+console.log(a);
+a = otherDate.getDate();
+console.log(a);
+a = otherDate.getMinutes();
+console.log(a);
+otherDate = new Date('June 13 1976');
+console.log(otherDate);
+otherDate = new Date('09/16/1976');
+console.log(otherDate);
+
+a = otherDate.getSeconds();
+console.log(a);
+a = otherDate.getHours();
+console.log(a);
+a = otherDate.getTime();
+console.log(a);
+a = otherDate.getMilliseconds();
+console.log(a);
+a = otherDate.getMonth();
+console.log(a);
+otherDate.setDate(23);
+console.log(otherDate);
+otherDate.setMonth(0);
+console.log(otherDate);
+otherDate.setFullYear(1900);
+console.log(otherDate);
+otherDate.setMinutes(2);
+otherDate.setHours(1);
+otherDate.setSeconds(3);
+console.log(otherDate);
+ 
