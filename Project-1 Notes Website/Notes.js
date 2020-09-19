01fac7eb11cf7e6668c0b8094c10f08ff2d10936
RiyaGaur <riyagaur15@gmail.com>
2020-09-19 21:14:02 +0530

    Magic Notes

--- Project-1 Notes Website/Notes.js
+++ Project-1 Notes Website/Notes.js
@@ -4,15 +4,22 @@ showNotes();
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function(e) {
   let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
   }
-  notesObj.push(addTxt.value);
+  let myObj = {
+    title: addTitle.value,
+    text: addTxt.value
+  }
+  notesObj.push(myObj);
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
+  addTitle.value = "";
+//   console.log(notesObj);
   showNotes();
 });
 
@@ -27,12 +34,11 @@ function showNotes() {
   let html = "";
   notesObj.forEach(function(element, index) {
     html += `
-            <div class="noteCard my-2 mx-2 card" id="ournotes" style="width: 20rem;">
+            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title">Note ${index + 1}</h5>
-                        <p class="card-text"> ${element}</p>
-                        <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
-                        <button id="${index}" onclick="MarkNote(this.id)" class="btn btn-primary">Mark Important</button>
+                        <h5 class="card-title">${element.title}</h5>
+                        <p class="card-text"> ${element.text}</p>
+                        <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                     </div>
                 </div>`;
   });
@@ -46,6 +52,8 @@ function showNotes() {
 
 // Function to delete a note
 function deleteNote(index) {
+//   console.log("I am deleting", index);
+
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
@@ -58,18 +66,12 @@ function deleteNote(index) {
   showNotes();
 }
 
-function MarkNote(index){
-  document.getElementById("ournotes").style.backgroundColor = "lightblue";
-}
-//document.querySelector('#notes').addEventListener('click' , function(e){
-  //alert("hiii");
-//  document.getElementById("ournotes").style.backgroundColor = "lightblue";
-//})
 
 let search = document.getElementById('searchTxt');
 search.addEventListener("input", function(){
 
     let inputVal = search.value.toLowerCase();
+    // console.log('Input event fired!', inputVal);
     let noteCards = document.getElementsByClassName('noteCard');
     Array.from(noteCards).forEach(function(element){
         let cardTxt = element.getElementsByTagName("p")[0].innerText;
@@ -79,5 +81,14 @@ search.addEventListener("input", function(){
         else{
             element.style.display = "none";
         }
+        // console.log(cardTxt);
     })
 })
+
+/*
+Further Features:
+1. Add Title
+2. Mark a note as Important
+3. Separate notes by user
+4. Sync and host to web server 
+*/
